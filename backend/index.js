606d6dbd44221637a477f5bcb6b6991b11d6c1b7
kayamazlum <mazlum09kaya@gmail.com>
2024-11-20 18:05:29 +0300

    gerekli paketler ve klasorler kuruldu

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cookieParser());
+
+app.get("/products", (req, res) => {
+  res.status(200).json({ message: "rota belirlendi" });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log("server is running on port 4000");
+});
